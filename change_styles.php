0408b3419dc8f0a03202a42627e10a0d1d4ec1a5
Matthew Guernsey <m.guernsey@outlook.com>
2020-08-14 19:58:55 -0600

    Styles can change.

--- change_styles.php
+++ change_styles.php
@@ -1,3 +1,27 @@
 <?php
 include "header.php";
+
+
+?>
+
+<form action="change_styles" method="POST">
+    <input type="radio" name="style" id="def" value="style.css">
+    <label for="def">Default Style</label>
+    <input type="radio" name="style" id="mint" value="style2.css">
+    <label for="mint">Mint Style</label>
+    <input type="radio" name="style" id="colorful" value="style3.css">
+    <label for="colorful">Colorful Style</label>
+    <input type="submit">
+</form>
+
+<?php
+function style(){
+    $_SESSION["styles"] = $_POST["style"];
+    header("Location: change_styles.php"); /* Redirect browser, MUST occur before anything is output to page */
+    exit();
+}
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+    style();
+} 
 ?>
